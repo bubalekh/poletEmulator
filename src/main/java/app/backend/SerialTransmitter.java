fd8331c2795fd8622ff3b739c490617164b32158
UserXgen <aleksey.pavlov2013@mail.ru>
2022-07-18 05:01:27 +0300

    added transfer to port

--- src/main/java/app/backend/SerialTransmitter.java
+++ src/main/java/app/backend/SerialTransmitter.java
@@ -1,6 +1,8 @@
 package app.backend;
 
 import app.backend.models.Parameter;
+import jssc.SerialPort;
+import jssc.SerialPortException;
 
 import java.util.List;
 
@@ -10,17 +12,25 @@ public class SerialTransmitter extends PackageInterface {
     public byte[] process(List<Parameter> parameters) {
         byte[] b_package = super.process(parameters);
 
-        /*
-        SerialPort transmitter = new SerialPort("COM4");
+        SerialPort transmitter = new SerialPort("COM1");
 
+        try {
+            transmitter.openPort();
+        } catch (SerialPortException e) {
+            throw new RuntimeException(e);
+        }
 
         try {
             transmitter.writeBytes(b_package);
         } catch (SerialPortException e) {
             throw new RuntimeException(e);
         }
-        */
 
+        try {
+            transmitter.closePort();
+        } catch (SerialPortException e) {
+            throw new RuntimeException(e);
+        }
 
         return b_package;
     }
